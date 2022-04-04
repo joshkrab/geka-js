8b867daba60e55dda746670baeadbffe7c1dcd57
joshkrab <neizvestnyi.igor@gmail.com>
2022-04-04 21:15:31 +0300

    jolp

--- 40 js-addEventListener/js/script.js
+++ 40 js-addEventListener/js/script.js
@@ -1,4 +1,3 @@
-console.log('check');
 // LinkedIn tasks _____________________________________________________
 let l1 = 10;
 let l2 = 0;
@@ -50,3 +49,72 @@ function deepEqual(obj1, obj2) {
 console.log(deepEqual(a, b));
 
 // ___________________________________________________________________________
+
+// Use the DOM object
+const button = document.querySelector('.button');
+button.onclick = function () {
+   console.log('Click Too');
+};
+
+function clickToo() {
+   console.log('Click 3');
+}
+
+const but3 = document.querySelector('.button3');
+but3.onclick = clickToo;
+
+// Недолік цих засобів, яки указані вище, що не можна повісити декілька
+// обробників - наступний перезапише попередній
+
+// Тому використовують наступні методи:
+document.querySelector('.button4').addEventListener('click', function (e) {
+   console.log('addEventListener');
+});
+document.querySelector('.button4').addEventListener('click', function (e) {
+   console.log('addEventListener2');
+}); // Two events in console
+
+function showEvent() {
+   console.log('function Event');
+}
+function showEvent2() {
+   console.log('function Event');
+}
+
+document.querySelector('.button4').addEventListener('click', showEvent);
+document.querySelector('.button4').removeEventListener('click', showEvent);
+
+// With options:
+
+const option = {
+   'capture': false, // фаза, на яку спрацює обробник
+   'once': false, // if true - обробник буде видален після виконання
+   'passive': false, // if true - обробник ніколи не визове preventDefault()
+};
+document.querySelector('.button4').addEventListener('click', showEvent, option);
+
+// Об'єкт опцій можна писати прям у дужках
+document.querySelector('.button4').addEventListener('click', showEvent2, {
+   'capture': false,
+   'once': true,
+   'passive': false,
+});
+
+// Об'єкт події _____________________________________________________________________
+// Коли відбувається подія, браузер створює об'єкт події,
+// записує туди деталі, передає в якості аргумента функції обробнику
+
+function showEvObj(event) {
+   console.log(event.type); // Тип події
+   console.log(event.target); // Об'єкт на якому спрацював обробник
+   console.log(event.currentTarget); // Об'єкт якому призначений обробник
+   console.log(event.clientX, event.clientY); // Координати курсору
+
+   console.log(event); // Усі деталі
+}
+
+document.querySelector('.button5').addEventListener('click', showEvObj);
+// При наведенні миші:
+// document.querySelector('.button5').addEventListener('mouseenter', showEvObj);
+
+// 12:30
