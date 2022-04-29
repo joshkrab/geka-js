e402f1103caf1c83af4eddd84221e3a5d7b0d403
joshkrab <neizvestnyi.igor@gmail.com>
2022-04-29 20:31:46 +0300

    met

--- 34 js-objects/js/script.js
+++ 34 js-objects/js/script.js
@@ -626,10 +626,41 @@ UserClient.prototype.newKey2 = 'privet Roma2'; // Додали в прототи
 
 let userCC = new UserClient('Вася');
 console.log(userCC);
-userCC.sayHi(); // Вася
+// userCC.sayHi(); // Вася
 
 // ______________________________________________________________________________
 // JSON - JavaScript Object Notation - формат файла для обмена данными
 // считается удобным для чтения и сохранения, как замена xml файлам
 
 //YAML - более сложный JSON
+
+// Задачка от Ромкі, та від Іпаму: ----------------------------------------------------------------------------
+
+// Реализуйте класс MyString, который будет иметь следующие методы:
+// метод reverse(), который параметром принимает строку, а возвращает ее в перевернутом виде,
+// метод ucFirst(), который параметром принимает строку, а возвращает эту же строку,
+// сделав ее первую букву заглавной
+// и метод ucWords, который принимает строку и делает заглавной первую букву каждого слова
+// этой строки.
+
+class MyString {
+   constructor() {}
+   reverse(str) {
+      return str.split('').reverse().join('');
+   }
+   ucFirst(str) {
+      return str[0].toUpperCase() + str.slice(1);
+   }
+   ucWords(str) {
+      return str
+         .split(' ')
+         .map((word) => this.ucFirst(word))
+         .join(' ');
+   }
+}
+
+var str = new MyString();
+
+console.log(str.reverse('abcde')); //выведет 'edcba'
+console.log(str.ucFirst('abcde')); //выведет 'Abcde'
+console.log(str.ucWords('abcde abcde abcde')); //выведет 'Abcde Abcde Abcde'
