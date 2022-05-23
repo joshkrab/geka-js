0d13166f659a965a248161be3e973b1c7764690b
joshkrab <neizvestnyi.igor@gmail.com>
2022-05-23 16:26:49 +0300

    do

--- 43 formToEmail/script.js
+++ 43 formToEmail/script.js
@@ -5,10 +5,136 @@ document.addEventListener('DOMContentLoaded', function () {
    console.log('ДОМ завантажився');
 
    const form = document.getElementById('form');
+
+   // Подія на відправку форми:
    form.addEventListener('submit', formSend);
 
    async function formSend(e) {
-      e.preventDefault();
+      e.preventDefault(); // відправку за замовчанням прибираємо
+
+      // Робимо валідацію полів:
+      let error = formValidate(form); // Записуємо результат функції в змінну
+
+      let formData = new FormData(form); // Отримуємо дані полів форми
+      formData.append('image', formImage.files[0]); // Додаємо зображення в змінну, як властивість
+
+      if (error === 0) {
+         // Повідомлення користувачу, що йде відправка форми:
+         form.classList.add('sending');
+
+         // Асінхронно відправляємо форму за допомогою фетч:
+         // await - чекаємо виконання відправки
+         let response = await fetch('sendmail.php', {
+            method: 'POST', // цим методом
+            body: formData, // цих даних в пхп файл sendmail.php
+         });
+
+         if (response.ok) {
+            let result = await response.json();
+            alert(result.message);
+            formPreview.innerHTML = ''; // Очищаємо превью фото
+            form.reset(); // Очищаємо усі поля форми
+            form.classList.remove('sending');
+         } else {
+            alert('Помилка!');
+            form.classList.remove('sending');
+         }
+      } else {
+         alert(`Заповніть обов'язкові поля`);
+      }
+   }
+
+   function formValidate(form) {
+      let error = 0;
+      // Adding required fields:
+      let formReq = document.querySelectorAll('.req'); // Колекція обов. полей
+
+      for (let index = 0; index < formReq.length; index++) {
+         const input = formReq[index];
+
+         // Перед перевіркой прибираємо класи .error
+         formRemoveError(input);
+
+         if (input.classList.contains('email')) {
+            if (emailTest(input)) {
+               // Налаштовано на фолс з !
+               formAddError(input);
+               error++;
+            }
+         } else if (
+            // Додаємо помилку, якщо обов'язковий чекбокс не обраний:
+            input.getAttribute('type') === 'checkbox' &&
+            input.checked === false
+         ) {
+            formAddError(input);
+            error++;
+         } else {
+            // Перевірка чи заповнено поле:
+            if (input.value === '') {
+               formAddError(input);
+               error++;
+            }
+         }
+      }
+      return error;
+   }
+
+   function formAddError(input) {
+      input.parentElement.classList.add('error');
+      input.classList.add('error');
+   }
+
+   function formRemoveError(input) {
+      input.parentElement.classList.remove('error');
+      input.classList.remove('error');
+   }
+
+   // Функция теста email
+   function emailTest(input) {
+      return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+   }
+
+   // ------ Предпросмотр імеджу ------------------------------------------------------
+
+   // Отримуємо в константу об'єкт з зображенням: наш інпут, тип файл:
+   const formImage = document.getElementById('formImage');
+   // Блок виводу картинки, пустий дів:
+   const formPreview = document.getElementById('formPreview');
+
+   // Слухаємо зміни в інпуті файл:
+   formImage.addEventListener('change', function (event) {
+      uploadFile(formImage.files[0]); // Запускаємо функцію з нашим файлом імеджем
+   });
+
+   function uploadFile(file) {
+      console.log(file.type); // в такому вигляді записує тип image/jpeg
+      // jpg записує jpeg
+
+      // Перевіряємо тип файлів:
+      if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+         alert('Дозволені тільки зображення!');
+         formImage.value = ''; // Очищуємо поле
+         return;
+      }
+      // Перевіряємо розмір файла:
+      console.log(file.size); // розмір в байтах наче
+      if (file.size > 2 * 1024 * 1024) {
+         alert('Файл має бути меньш 2мб');
+         formImage.value = ''; // Очищуємо поле
+         return;
+      }
+      // Об'єкт FileReader дозволяє веб-додаткам асінхронно читати вміст файлів
+      var reader = new FileReader();
+      reader.onload = function (e) {
+         // Вставляємо отриманий результат від завантаження файла:
+         formPreview.innerHTML = `<img src="${e.target.result}" alt="Photo">`;
+      };
+      reader.onerror = function () {
+         alert('Помилка');
+      };
+      // readAsDataURL() - метод об'єкту FileReader який читає файл і
+      // записує результат у властивість FileReader.result у вигляді Data URL,
+      // який представляє дані файлу як рядкок в Base64 кодуванні.
+      reader.readAsDataURL(file);
    }
 });
-// 31min
---
+++ 43 formToEmail/sendmail.php
@@ -0,0 +1,75 @@
+<?php
+
+// Ці 4 рядки підключають пхп плагін:
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+// Створення об'єкту плагіна та налаштування кодування:
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHTML(true); // Можливість хтмл тегів в листі
+
+//От кого письмо
+$mail->setFrom('josh.animato@gmail.com', 'Фрилансер по жизни');
+//Кому отправить
+$mail->addAddress('joshkrab@gmail.com');
+//Тема письма
+$mail->Subject = 'Привіт, це Ігор гітарист';
+
+//Рука
+$hand = "Правая";
+if ($_POST['hand'] == "left") {
+   $hand = "Левая";
+}
+
+//Тело письма
+$body = '<h1>Встречайте супер письмо!</h1>';
+
+
+// Перевірки полей теж у пхп, що не порожні:
+if (trim(!empty($_POST['name']))) {
+   $body .= '<p><strong>Имя:</strong> ' . $_POST['name'] . '</p>';
+}
+if (trim(!empty($_POST['email']))) {
+   $body .= '<p><strong>E-mail:</strong> ' . $_POST['email'] . '</p>';
+}
+if (trim(!empty($_POST['hand']))) {
+   $body .= '<p><strong>Рука:</strong> ' . $hand . '</p>';
+}
+if (trim(!empty($_POST['age']))) {
+   $body .= '<p><strong>Возраст:</strong> ' . $_POST['age'] . '</p>';
+}
+
+if (trim(!empty($_POST['message']))) {
+   $body .= '<p><strong>Сообщение:</strong> ' . $_POST['message'] . '</p>';
+}
+
+//Прикрепить файл
+if (!empty($_FILES['image']['tmp_name'])) {
+   //путь загрузки файла
+   $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+   //грузим файл
+   if (copy($_FILES['image']['tmp_name'], $filePath)) {
+      $fileAttach = $filePath;
+      $body .= '<p><strong>Фото в приложении</strong>';
+      $mail->addAttachment($fileAttach);
+   }
+}
+
+$mail->Body = $body;
+
+//Отправляем
+if (!$mail->send()) {
+   $message = 'Ошибка';
+} else {
+   $message = 'Данные отправлены!';
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
