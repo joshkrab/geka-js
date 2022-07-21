4cba8615efbb6d29feb5d6bdfec80a203a221865
joshkrab <neizvestnyi.igor@gmail.com>
2022-07-21 18:25:32 +0300

    not

--- codeWars/kata.js
+++ codeWars/kata.js
@@ -626,6 +626,64 @@ function DNAStrand(dna) {
 }
 console.log(DNAStrand('ATTGC'));
 // ------------------------------------------------------------------------------------------------------------------------
+// Розробити функцію по операціям по ключевим словам: seven(times(five())); // must return 35
+
+function zero(func) {
+   return func ? func(0) : 0;
+}
+function one(func) {
+   return func ? func(1) : 1;
+}
+
+function three(func) {
+   return func ? func(3) : 3;
+}
+function four(func) {
+   return func ? func(4) : 4;
+}
+function five(func) {
+   return func ? func(5) : 5;
+}
+
+function seven(func) {
+   return func ? func(7) : 7;
+}
+function eight(func) {
+   return func ? func(8) : 8;
+}
+function nine(func) {
+   return func ? func(9) : 9;
+}
+
+function plus(b) {
+   return function (a) {
+      return a + b;
+   };
+}
+function minus(b) {
+   return function (a) {
+      return a - b;
+   };
+}
+function times(b) {
+   return function (a) {
+      return a * b;
+   };
+}
+
+function two(func) {
+   return func ? func(2) : 2;
+}
+function six(func) {
+   return func ? func(6) : 6;
+}
+function dividedBy(b) {
+   return function (a) {
+      return Math.floor(a / b);
+   };
+}
+console.log(six(dividedBy(two())));
+
 // ------------------------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------------------------------
